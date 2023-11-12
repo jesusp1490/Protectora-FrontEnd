31a46d81be08235cb95140f0359ddde2128fa004
Jesus Perez <jesusperez_14@hotmail.com>
2023-11-12 11:07:59 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage/LoginPage';
+import RegisterPage from './Pages/RegisterPage.jsx/RegisterPage';
+import UserProfilePage from './Pages/UserProfilePage.jsx/UserProfilePage';
 
-function App() {
+const App = () => {
+  const isAuthenticated = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+        <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/profile" element={<UserProfilePage />} />
+        </Routes>
+    </Router>
+);
+};
+
+const PrivateRoute = ({ element: Element, isAuthenticated, ...rest }) => (
+  isAuthenticated ? <Route {...rest} element={<Element />} /> : <Navigate to="/login" replace />
+);
 
 export default App;
+
+
+
---
+++ src/Pages/RegisterPage.jsx/RegisterPage.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Register = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [gender, setGender] = useState('');
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:5055/users/register', {
+                email,
+                username,
+                password,
+                gender,
+            });
+
+            const data = response.data;
+
+            if (data.success) {
+                console.log('Registro exitoso');
+            } else {
+                console.error(data.message);
+                alert('El registro ha fallado. Asegúrate de que todos los campos estén llenos correctamente.');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleRegister}>
+            <div className="inputbox">
+                <ion-icon name="mail-outline"></ion-icon>
+                <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="form-field"
+                />
+                <label htmlFor="" className="form-label">
+                    Email:
+                </label>
+            </div>
+
+            <div className="inputbox">
+                <ion-icon name="person-outline"></ion-icon>
+                <input
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    className="form-field"
+                />
+                <label htmlFor="" className="form-label">
+                    Username:
+                </label>
+            </div>
+
+            <div className="inputbox">
+                <ion-icon name="lock-closed-outline"></ion-icon>
+                <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="form-field"
+                />
+                <label htmlFor="" className="form-label">
+                    Password:
+                </label>
+            </div>
+
+            <div className="inputbox">
+                <ion-icon name="male-female-outline"></ion-icon>
+                <select
+                    name="gender"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                    required
+                    className="form-field"
+                >
+                    <option value="Female">Female</option>
+                    <option value="Male">Male</option>
+                </select>
+            </div>
+
+            <button type="submit">Register</button>
+        </form>
+    );
+};
+
+export default Register;
