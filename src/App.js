5dfc315a5be839306eb51d116cc27130d53dcbad
paulagimeno <paula-gl-98@hotmail.com>
2023-11-14 10:10:16 +0100

    register pet page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import LoginPage from './Pages/LoginPage/LoginPage';
 import RegisterPage from './Pages/RegisterPage.jsx/RegisterPage';
 import UserProfilePage from './Pages/UserProfilePage.jsx/UserProfilePage';
+import RegisterPet from './Pages/RegisterPet/RegisterPet';
 
 const App = () => {
   const isAuthenticated = true;
@@ -13,6 +14,7 @@ const App = () => {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/profile" element={<UserProfilePage />} />
+            <Route path="/register-pet" element={<RegisterPet />}/>
         </Routes>
     </Router>
 );
