c05864f876154c29affb73bd6dadcbdfda493876
Jesus Perez <jesusperez_14@hotmail.com>
2023-11-16 18:34:47 +0100

    HomePage and fighting with carousel

--- src/Pages/UserProfilePage/UserProfilePage.jsx
+++ src/Pages/UserProfilePage/UserProfilePage.jsx
@@ -7,6 +7,7 @@ const defaultProfilePic = 'https://res.cloudinary.com/dizd9f3ky/image/upload/v17
 
 const UserProfile = () => {
 
+    
     const profilePic = localStorage.getItem('userProfile');
 
     const userImage = profilePic || defaultProfilePic;
@@ -14,20 +15,22 @@ const UserProfile = () => {
     return (
         <div className='userProfile-container'>
             <div className='userImg-container'>
-                <img src={userImage} alt='User Foto'/>
+                <img src={userImage} alt='User Foto' className='user-img'/>
             </div>
 
             <div className='userProfile-menu'>
                 <div>
-                    <div className='miPerfil'>
+                    <div className='userProfile-option'>
                         <Link to="/profile/mi-perfil" >
-                            <button className="userProfile-option" type="button">Mi Perfil<img src='https://res.cloudinary.com/dizd9f3ky/image/upload/v1699831804/arrow_2x_cszuw5.png' alt='arrow' /></button>
+                            <button className="miPerfil" type="button">Mi Perfil<img src='https://res.cloudinary.com/dizd9f3ky/image/upload/v1699831804/arrow_2x_cszuw5.png' alt='arrow' className='arrow'/></button>
                         </Link>
                     </div>
 
-                    <Link to="/profile/direcciones" >
-                        <button className="userProfile-option" type="button">Direcciones</button>
-                    </Link>
+                    <div className='direcciones'>
+                        <Link to="/profile/direcciones" >
+                            <button className="userProfile-option" type="button">Direcciones</button>
+                        </Link>
+                    </div>
 
                     <Link to="/profile/favoritos" >
                         <button className="userProfile-option" type="button">Favoritos</button>
