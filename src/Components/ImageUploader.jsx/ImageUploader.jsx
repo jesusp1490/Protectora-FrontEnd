6c2ce8f40fef1c292b9ab586435dc41fc4e041a2
cesarbet <cesarbet@gmail.com>
2023-11-22 11:25:02 +0100

    ImageUploader-Correction

--- src/Components/ImageUploader.jsx/ImageUploader.jsx
+++ src/Components/ImageUploader.jsx/ImageUploader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import './_ImageUploader.scss'
 
-const ImageUploader = ({ onImageChange, borderStyle = '1px dashed #ccc' }) => {
+const ImageUploader = ({ onImageChange, index, borderStyle = '1px dashed #ccc' }) => {
     const [image, setImage] = useState(null);
 
     const handleImageChange = (event) => {
@@ -11,32 +11,34 @@ const ImageUploader = ({ onImageChange, borderStyle = '1px dashed #ccc' }) => {
             const reader = new FileReader();
             reader.onload = () => {
                 setImage(reader.result);
-                onImageChange(reader.result); 
+                onImageChange(index, reader.result);
             };
             reader.readAsDataURL(file);
         }
     };
 
-
     return (
         <div className='infoA-ImgUp-container'>
-            <label htmlFor="imageInput">
+            <label htmlFor={`imageInput${index}`}>
                 {image ? (
                     <img src={image} alt="Preview" style={{ width: '120px', height: '160px' }} />
                 ) : (
                     <div className="infoA-placeholder">
-                        <img src='https://res.cloudinary.com/dizd9f3ky/image/upload/v1700436714/group7_2x_gtbukn.png' alt="Camera Icon" className='infoA-ImgUp-img' />
+                        <img
+                            src='https://res.cloudinary.com/dizd9f3ky/image/upload/v1700436714/group7_2x_gtbukn.png'
+                            alt="Camera Icon"
+                            className='infoA-ImgUp-img'
+                        />
                         <p className='infoA-ImgUp-p'>Subir foto</p>
                     </div>
                 )}
             </label>
             <input
                 type="file"
-                id="imageInput"
+                id={`imageInput${index}`}
                 style={{ display: 'none' }}
                 onChange={(event) => handleImageChange(event)}
             />
-
         </div>
     );
 };
